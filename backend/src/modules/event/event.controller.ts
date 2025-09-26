fe29482c0738e1661a5b7eb7b0f3e55491b1d2be
tientran <kimtientran0410@gmail.com>
2025-09-26 10:41:12 +0700

    Modify logic Module CRUD Event

--- backend/src/modules/event/event.controller.ts
+++ backend/src/modules/event/event.controller.ts
@@ -16,12 +16,15 @@ import { EventUpdateDto } from './dto/event-update.dto';
 import { EventDto } from './dto/event.dto';
 import { UserDto } from '../user/dto/user.dto';
 import type { Uuid } from 'src/common/types';
+import { RequireAdmin, RequireLoggedIn } from 'src/guards/role-container';
 
 @Controller('events')
 export class EventController {
   constructor(private readonly eventService: EventService) {}
 
   @Post()
+  @RequireLoggedIn()
+  @RequireAdmin()
   async create(@Body() eventCreateDto: EventCreateDto): Promise<EventDto> {
     return EventDto.fromDomain(
       await this.eventService.create(
@@ -32,22 +35,21 @@ export class EventController {
 
   @Get()
   async findAll(): Promise<EventDto[]> {
-    return EventDto.fromDomains(
-      await this.eventService.findAll()
-    );
+    return EventDto.fromDomains(await this.eventService.findAll());
   }
 
   @Get(':id')
   async findOne(@Param('id') id: Uuid): Promise<EventDto> {
-    return EventDto.fromDomain(
-      await this.eventService.findOne(id));
+    return EventDto.fromDomain(await this.eventService.findOne(id));
   }
 
   @Put(':id')
+  @RequireLoggedIn()
+  @RequireAdmin()
   async update(
     @Param('id') id: Uuid,
     @Body() eventUpdateDto: EventUpdateDto,
-  ) {
+  ): Promise<EventDto> {
     return EventDto.fromDomain(
       await this.eventService.update(
         id,
@@ -57,6 +59,8 @@ export class EventController {
   }
 
   @Delete(':id')
+  @RequireLoggedIn()
+  @RequireAdmin()
   @HttpCode(HttpStatus.NO_CONTENT)
   async remove(@Param('id') id: Uuid) {
     return this.eventService.remove(id);
